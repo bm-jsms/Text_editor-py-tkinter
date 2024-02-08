9505cc701626aea29ef6d5fabd1290899e136c2c
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-08 17:49:01 -0500

    class editor - exit

--- app.py
+++ app.py
@@ -5,8 +5,22 @@ from tkinter import filedialog, messagebox
 root = tk.Tk()
 
 
+class simpleTextEditor:
+    def __init__(self, root):
+        self.root = root
+        self.text_area = tk.Text(self.root, bg="black", fg="white")
+        self.text_area.pack(expand=True, fill="both")
+
+    def quit_confirm(self):
+        if messagebox.askokcancel("Exit", "Do you want to quit?"):
+            root.quit()
+
+
 root.title("Code Editor")
 root.geometry("800x600")
+
+
+editor = simpleTextEditor(root)
 
 
 bar_menu = tk.Menu(root)
@@ -17,7 +31,7 @@ root.config(bg="black")
 menu_options.add_command(label="New")
 menu_options.add_command(label="Open")
 menu_options.add_command(label="Save")
-menu_options.add_command(label="Exit", command=root.quit)
+menu_options.add_command(label="Exit", command=editor.quit_confirm)
 
 root.config(menu=bar_menu)
 bar_menu.add_cascade(label="File", menu=menu_options)
