891baf110c37a6e7fe63f9420ef09bbaf7e34e0d
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-08 17:58:11 -0500

    class editor - open

--- app.py
+++ app.py
@@ -8,8 +8,16 @@ root = tk.Tk()
 class simpleTextEditor:
     def __init__(self, root):
         self.root = root
-        self.text_area = tk.Text(self.root, bg="black", fg="white")
+        self.text_area = tk.Text(self.root, bg="#000015", fg="white")
         self.text_area.pack(expand=True, fill="both")
+
+    def open_file(self):
+        file = filedialog.askopenfile(
+            parent=root, mode="rb", title="Select a file")
+        if file != None:
+            contents = file.read()
+            self.text_area.insert("1.0", contents)
+            file.close()
 
     def quit_confirm(self):
         if messagebox.askokcancel("Exit", "Do you want to quit?"):
@@ -29,7 +37,7 @@ menu_options = tk.Menu(bar_menu, tearoff=0)
 root.config(bg="black")
 
 menu_options.add_command(label="New")
-menu_options.add_command(label="Open")
+menu_options.add_command(label="Open", command=editor.open_file)
 menu_options.add_command(label="Save")
 menu_options.add_command(label="Exit", command=editor.quit_confirm)
 
