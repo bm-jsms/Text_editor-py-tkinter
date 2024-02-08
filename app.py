6a7252b75fd31eb99834bfc416812c3307568a1f
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-08 18:08:19 -0500

    class editor - new, save

--- app.py
+++ app.py
@@ -11,12 +11,22 @@ class simpleTextEditor:
         self.text_area = tk.Text(self.root, bg="#000015", fg="white")
         self.text_area.pack(expand=True, fill="both")
 
+    def new_file(self):
+        self.text_area.delete(1.0, tk.END)
+
     def open_file(self):
         file = filedialog.askopenfile()
         if file:
             self.text_area.delete(1.0, tk.END)
             with open(file.name, "r") as f:
                 self.text_area.insert(1.0, f.read())
+
+    def save_file(self):
+        file = filedialog.asksaveasfile(mode="w")
+        if file:
+            data = self.text_area.get(1.0, tk.END)
+            file.write(data)
+            file.close()
 
     def quit_confirm(self):
         if messagebox.askokcancel("Exit", "Do you want to quit?"):
@@ -35,9 +45,9 @@ menu_options = tk.Menu(bar_menu, tearoff=0)
 
 root.config(bg="black")
 
-menu_options.add_command(label="New")
+menu_options.add_command(label="New", command=editor.new_file)
 menu_options.add_command(label="Open", command=editor.open_file)
-menu_options.add_command(label="Save")
+menu_options.add_command(label="Save", command=editor.save_file)
 menu_options.add_command(label="Exit", command=editor.quit_confirm)
 
 root.config(menu=bar_menu)
