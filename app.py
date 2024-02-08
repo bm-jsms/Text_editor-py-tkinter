5b51f659986b19e511a440b5584edca1cba3fced
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-08 17:18:19 -0500

    bar menu

--- app.py
+++ app.py
@@ -9,4 +9,17 @@ root.title("Code Editor")
 root.geometry("800x600")
 
 
+bar_menu = tk.Menu(root)
+menu_options = tk.Menu(bar_menu, tearoff=0)
+
+root.config(bg="black")
+
+menu_options.add_command(label="New")
+menu_options.add_command(label="Open")
+menu_options.add_command(label="Save")
+menu_options.add_command(label="Exit", command=root.quit)
+
+root.config(menu=bar_menu)
+bar_menu.add_cascade(label="File", menu=menu_options)
+
 root.mainloop()
